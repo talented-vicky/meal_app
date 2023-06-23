7502c9c5f88dcc184840580db93db1986adcf86f
Victor Otubure <victorotubure7@gmail.com>
2023-06-23 13:36:32 -0700

    intiliatized a new repo for a meal app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("stuff");
+  }
+}
+
+// SHAKA ilembe
+// I surrender all to you | i give myself away | Daily as I live | You are the reason I live | Lord I give you my heart => EBEZENA
+
+// downloads
+// the other side --Luli
+// mp3
+// ZAYN (2), Maroon 5 (2)
+// am I wrong/
+
+/* 
+to dream withoug borders or boundaries, to seek adventure,
+to obsess and create => the world IMPOSSIBLE doesn't have to exist
+*/
